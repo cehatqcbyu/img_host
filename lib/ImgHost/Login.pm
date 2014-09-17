2e2a842b5fc20dc466fbc5105e51e7ec571ba89a
cehatqcbyu <cehatqcbyu@outlook.com>
2014-09-17 14:20:30 +0300

    17/09/2014 14:20

--- lib/ImgHost/Login.pm
+++ lib/ImgHost/Login.pm
@@ -6,7 +6,50 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub login {
     my $self = shift;
-    $self->render;
+
+    # Query or POST parameters
+    my $user = $self->param('user') || '';
+    my $pass = $self->param('pass') || '';
+
+    # Check password and render "index.html.ep" if necessary
+    return $self->render unless $self->users->check($user, $pass);
+
+    # Store username in session
+    $self->session(user => $user);
+    # Redirect to protected page with a 302 response
+    $self->redirect_to('/images');
+}
+
+sub registration {
+    my $self = shift;
+
+    # Query or POST parameters
+    my $user = $self->param('user') || '';
+    my $pass = $self->param('pass') || '';
+    my $pass2 = $self->param('pass2') || '';
+    my $err_message = ''; # show user if something wrong
+
+    # if everyhing is blank
+    return $self->render(err_message => $err_message) if( ($user eq '') && ($pass eq '') && ($pass2 eq '') );
+
+    # register new user
+    my $result = $self->users->register($user, $pass, $pass2);
+
+    # registration succesfull
+    if ($result eq 'ok') {
+        if (mkdir ("images/$user")) {
+            $self->redirect_to('index');
+        }
+        else {
+            $err_message = "Unknown error!";
+            return $self->render(err_message => $err_message);
+        }
+    }
+    else {
+        $err_message = $result;
+        return $self->render(err_message => $err_message);
+    }
+
 }
 
 1;
